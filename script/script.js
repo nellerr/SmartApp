e1986ec7d682551552eb077e293e070609e2300d
nellerr <nellerr@mail.ru>
2020-02-26 12:40:59 +0200

    message

---
+++ script/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.galslider').slick({
+    
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<img src="images/arrow-left.png" class="prev">',
+    nextArrow: '<img src="images/arrow-right.png" class="next">',
+    
+
+    
+    
+    
+   
+  });
+})
